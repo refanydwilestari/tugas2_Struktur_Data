db9c4172a143d944f82eb2b864b009af1b299d9c
refanydwi <146598703+refanydwilestari@users.noreply.github.com>
2024-05-02 18:49:04 +0700

    Update maps.py

--- maps.py
+++ maps.py
@@ -1,1 +1,5 @@
-
+#Struktur Data
+#Kelompok 3
+#Nama : Ronald Budi Abdul Wahid   #NIM :23091397142
+#Nama : Muhammad Ibnu Nadhif      #NIM :23091397161
+#Nama : Refany Dwi Lestari        #NIM :23091397170
